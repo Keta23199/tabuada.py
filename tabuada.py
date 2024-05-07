d4e89b6f96eb02c2f38d2367ecc323190e0db87c
Keta23199 <165030072+Keta23199@users.noreply.github.com>
2024-05-07 09:32:02 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,87 @@
+console.log("1 * 1 = 1");
+console.log("1 * 2 = 2");
+console.log("1 * 3 = 3");
+console.log("1 * 4 = 4");
+console.log("1 * 5 = 5");
+console.log("1 * 6 = 6");
+console.log("1 * 7 = 7");
+console.log("1 * 8 = 8");
+console.log("1 * 9 = 9");
+console.log("1 * 10 = 10");
+
+console.log(" ");
+
+console.log("3 * 1 = 3");
+console.log("3 * 2 = 6");
+console.log("3 * 3 = 9");
+console.log("3 * 4 = 12");
+console.log("3 * 5 = 15");
+console.log("3 * 6 = 18");
+console.log("3 * 7 = 21");
+console.log("3 * 8 = 24");
+console.log("3 * 9 = 27");
+console.log("3 * 10 = 30");
+
+console.log(" ");
+
+console.log("7 * 1 = 7");
+console.log("7 * 2 = 14");
+console.log("7 * 3 = 21");
+console.log("7 * 4 = 28");
+console.log("7 * 5 = 35");
+console.log("7 * 6 = 42");
+console.log("7 * 7 = 49");
+console.log("7 * 8 = 56");
+console.log("7 * 9 = 63");
+console.log("7 * 10 = 70");
+
+console.log(" ");
+
+console.log("5 * 1 =","5" *"1");
+
+console.log ("5 * 1 = " ,5 * 1);
+
+console.log(" ");
+
+let t = 70;  
+
+console.log( t + "* 1 = ", t * 1);
+console.log( t + "* 2 = ", t * 2);
+console.log( t + "* 3 = ", t * 3);
+console.log( t + "* 4 = ", t * 4);
+console.log( t + "* 5 = ", t * 5);
+console.log( t + "* 6 = ", t * 6);
+console.log( t + "* 7 = ", t * 7);
+console.log( t + "* 8 = ", t * 8);
+console.log( t + "* 9 = ", t * 9 );
+console.log( t + "* 10 = ", t * 10);
+
+console.log(" ");
+
+t = t +1;
+
+console.log( t + "* 1 = ", t * 1);
+console.log( t + "* 2 = ", t * 2);
+console.log( t + "* 3 = ", t * 3);
+console.log( t + "* 4 = ", t * 4);
+console.log( t + "* 5 = ", t * 5);
+console.log( t + "* 6 = ", t * 6);
+console.log( t + "* 7 = ", t * 7);
+console.log( t + "* 8 = ", t * 8);
+console.log( t + "* 9 = ", t * 9 );
+console.log( t + "* 10 = ", t * 10);
+
+console.log(" ");
+
+t ++ ;
+
+console.log( t + "* 1 = ", t * 1);
+console.log( t + "* 2 = ", t * 2);
+console.log( t + "* 3 = ", t * 3);
+console.log( t + "* 4 = ", t * 4);
+console.log( t + "* 5 = ", t * 5);
+console.log( t + "* 6 = ", t * 6);
+console.log( t + "* 7 = ", t * 7);
+console.log( t + "* 8 = ", t * 8);
+console.log( t + "* 9 = ", t * 9 );
+console.log( t + "* 10 = ", t * 10);
